54941472ce38e7b1d04ae91846e95d33288537cf
Nadiia <nenadinweb@gmail.com>
2022-03-29 12:51:16 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {Container} from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+      <Container maxWidth="xl">
+        Users
+      </Container>
+    </div>
+  );
+}
+
+export default App;
